1b999b9efdf28bcd6cc9c8ed6b2d9f6bfac7dd5c
aibo <pt-aibo@mail.idcduty.com>
2024-07-04 19:07:13 +0800

    first commit

---
+++ components/CartItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex justify-start my-2">
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(['product','selectedArray'])
+const product = toRefs(props)
+</script>
